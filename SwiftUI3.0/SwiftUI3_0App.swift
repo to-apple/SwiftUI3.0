766b376b92db64950dab8a8b96310c4b1d291e4b
Shimoorook <jyoon@esri.com>
2022-01-14 20:24:07 -0800

    Initial Commit

---
+++ SwiftUI3.0/SwiftUI3_0App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI3_0App.swift
+//  SwiftUI3.0
+//
+//  Created by Juhan Yoon on 1/14/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI3_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
